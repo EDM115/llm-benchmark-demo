2a48a2ab97449909eb6b54db1ac1042e4ccc5925
EDM115 <dev@edm115.eu.org>
2025-09-09 14:34:54 +0200

    feat: added Sonoma Alpha models also fixed some TS errors along the way, added another Kimi K2 version and appropriately named the previous one

--- assets/FixedCode-CKZe7-Cf.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-n5h6x82I.js";const r={};function c(t,_){return"👀"}const o=e(r,[["render",c]]);export{o as default};
---
+++ assets/FixedCode-YoOCvcC8.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-B4B5s16d.js";const r={};function c(t,_){return"👀"}const o=e(r,[["render",c]]);export{o as default};
